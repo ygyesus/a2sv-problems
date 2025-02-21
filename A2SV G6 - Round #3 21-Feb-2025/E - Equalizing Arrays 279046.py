34426efc5c65a89115a4a4aeb57d5a3d202b5f69
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-02-21 19:37:26 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 21-Feb-2025/E - Equalizing Arrays 279046.py
@@ -0,0 +1,39 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())
+a = list(map(int, input().split()))
+
+m = int(input())
+b = list(map(int, input().split()))
+
+i=j=0
+flag = True
+
+a_sum = b_sum = 0
+length = 0
+if sum(a) != sum(b):
+    flag = False
+while i<len(a) and flag:
+
+    a_sum += a[i]
+    b_sum += b[j]
+    
+    i+=1
+    j+=1
+    
+    while a_sum != b_sum:
+        while a_sum < b_sum:
+            a_sum += a[i]
+            i+=1
+         
+
+        while b_sum < a_sum:
+            b_sum += b[j]
+            j+=1
+        
+    length += 1
+    
+if not flag:
+    print(-1)
+else:
+    print(length)
