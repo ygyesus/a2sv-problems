9061cb005143ecf4e941b0eda5b3e42f60724875
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-02-22 12:07:30 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 22-Feb-2025/F - Binary Substrings with Exactly k Ones 280196.py
@@ -0,0 +1,24 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+def findAtmost(k):
+    if k==-1:
+        return 0
+    left = 0
+    
+    total = 0
+    ans = 0
+    for right in range(len(s)):
+        total += int(s[right])
+        
+        while total > k:
+            total -= int(s[left])
+            left += 1
+            
+        ans += right-left+1
+        
+    return ans
+k = int(input())
+
+s = input()
+
+print(findAtmost(k) - findAtmost(k-1))
