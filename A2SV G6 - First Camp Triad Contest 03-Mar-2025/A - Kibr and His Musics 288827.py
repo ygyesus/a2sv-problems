e59056d5d3e2e0c0e443a1d172cca1d2ab4a715b
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-03-03 15:03:11 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 03-Mar-2025/A - Kibr and His Musics 288827.py
@@ -0,0 +1,25 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+n,m = map(int, input().split())
+
+prefix = []
+for i in range(n):
+    c,t = map(int, input().split())
+    
+    prefix.append(c*t)
+    if i>0:
+        prefix[i] += prefix[i-1]
+    
+    
+moments = list(map(int, input().split()))
+
+songIndex = 0
+for j in range(m):
+    
+    moment = moments[j]    
+    while prefix[songIndex]<moment:
+        songIndex += 1
+        
+    print(songIndex+1)
+        
+    
