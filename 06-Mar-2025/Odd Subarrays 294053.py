5872b4fe9a16b11c0e23ac050826bbd6357aa296
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-03-06 12:29:28 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 06-Mar-2025/Odd Subarrays 294053.py
@@ -0,0 +1,23 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    array = list(map(int, input().split()))
+    
+    ans = 0
+    minNum = array[0]
+    
+    i = 1
+    
+    while i<n:
+        if array[i] < array[i-1]:
+            ans += 1
+            i += 2
+            
+        else:
+            i += 1
+            
+    print(ans)
