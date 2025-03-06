714f0215f5a3b170f9b3540c6bdeb57bb2a3b191
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-03-06 18:34:30 +0300

    Created 'Daily Temperatures' by A2SV Hub

---
+++ 06-Mar-2025/Daily Temperatures 294813.py
@@ -0,0 +1,17 @@
+# Problem: Daily Temperatures - https://leetcode.com/problems/daily-temperatures/
+
+from collections import deque
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+      
+        stack = []
+
+        ans = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                ans[stack[-1]] = i-stack[-1]
+                stack.pop()
+
+            stack.append(i)
+
+        return ans
