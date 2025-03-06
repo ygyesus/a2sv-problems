c636b1b1343a20cf2a0bdc0a010c2670a2610a72
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-03-06 13:12:35 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 06-Mar-2025/D - Life Across the Stars 294089.py
@@ -0,0 +1,31 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+
+n = int(input())
+
+datesToFreq = defaultdict(int)
+for _ in range(n):
+    b,d = map(int, input().split())
+    
+    datesToFreq[b] += 1
+    datesToFreq[d] -= 1
+    
+    
+dates = datesToFreq.keys()
+dates = sorted(list(dates))
+
+freqs = [datesToFreq[date] for date in dates]
+for i in range(1, len(freqs)):
+    freqs[i] += freqs[i-1]
+    
+maxFreq = 0
+maxDate = -1
+
+for i,freq in enumerate(freqs):
+    if freq > maxFreq:
+        maxFreq = freq
+        maxDate = dates[i]
+    
+print(maxDate, maxFreq)
+    
