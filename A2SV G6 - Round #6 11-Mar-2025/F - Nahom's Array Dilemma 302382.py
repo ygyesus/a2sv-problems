322ed38ec43449a430a9ecdc379a271dca276dc4
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-03-11 15:11:30 +0300

    Created 'F - Nahom's Array Dilemma' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/F - Nahom's Array Dilemma 302382.py
@@ -0,0 +1,41 @@
+# Problem: F - Nahom's Array Dilemma - https://codeforces.com/gym/594077/problem/F
+
+t = int(input())
+
+def check(arr):
+
+    n = len(arr)
+    stack = []
+
+    total = 0
+
+    prefix = [0]
+    
+    for i in range(n):
+        prefix.append(prefix[-1] + arr[i])
+
+    # print(prefix)
+
+
+    for right in range(n):
+        while stack and arr[stack[-1]] <= arr[right]:
+            left = stack.pop()
+
+            total = prefix[right+1] - prefix[left]
+
+            if total > arr[right]:
+                return False
+
+        stack.append(right)
+
+    return True
+       
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    if check(a) and check(a[::-1]):
+        print("YES")
+    else:
+        print("NO")
+    
+    
