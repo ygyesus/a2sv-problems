7de9248e1d6bfa3038dbe61be6cf703fccd47906
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-03-30 07:56:42 +0300

    Created 'Heaters ' by A2SV Hub

---
+++ 30-Mar-2025/Heaters  321862.py
@@ -0,0 +1,38 @@
+# Problem: Heaters  - https://leetcode.com/problems/heaters/
+
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+
+        
+        def check(radius):
+            # two pointers
+
+            house, heater = 0,0
+            while house<len(houses) and heater<len(heaters):
+                if abs(houses[house] - heaters[heater]) > radius:
+                    heater += 1
+
+                else:
+                    house += 1
+
+            return house >= len(houses)
+
+        heaters.sort()
+        houses.sort()
+        
+        left,right = 0,10**9
+
+        ans = float('inf')
+        while left<=right:
+
+            radius = (left+right)//2
+
+            if check(radius):
+                ans = min(ans, radius)
+                right = radius - 1
+            else:
+                left = radius + 1
+
+        return ans
+
+            
