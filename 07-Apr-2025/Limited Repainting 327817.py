26155b4b1e00e2ce8ce9b429bf1f185234fb493f
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-04-07 13:14:31 +0300

    Created 'Limited Repainting' by A2SV Hub

---
+++ 07-Apr-2025/Limited Repainting 327817.py
@@ -0,0 +1,44 @@
+# Problem: Limited Repainting - https://codeforces.com/contest/2070/problem/C
+
+t = int(input())
+
+def check(penalty, s, a, k):
+    ans = []
+    for i, char in enumerate(s):
+        if a[i] > penalty:
+            ans.append(s[i])
+        else:
+            ans.append(None)
+
+    soFar = 'R'
+    ops = 0
+    for char in ans:
+        if soFar == 'R':
+            if char == 'B':
+                soFar = 'B'
+                ops += 1
+        elif soFar == 'B':
+            if char == 'R':
+                soFar = 'R'
+
+    return ops <= k
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    s = input().strip()
+    a = list(map(int, input().split()))
+    
+
+    left, right = 0, max(a)
+    penalty = None
+
+    while left <= right:
+        mid = (left + right) // 2
+        if check(mid, s, a, k):  # Pass k to check
+            penalty = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    print(penalty if penalty is not None else 0)
+#     print("===")
