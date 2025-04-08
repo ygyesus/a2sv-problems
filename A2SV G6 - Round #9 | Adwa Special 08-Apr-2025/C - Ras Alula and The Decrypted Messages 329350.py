ae8b65418c702a8c4c0f1c05f2b34b3fe5746e25
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-04-09 01:59:20 +0300

    Created 'C - Ras Alula and The Decrypted Messages' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 08-Apr-2025/C - Ras Alula and The Decrypted Messages 329350.py
@@ -0,0 +1,41 @@
+# Problem: C - Ras Alula and The Decrypted Messages - https://codeforces.com/gym/601269/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    n,m = list(map(int, input().split()))
+    s = list(input())
+    
+    for i in range(n):
+        s[i] = ord(s[i])
+        
+    w = list(input())
+    
+    for i in range(m):
+        w[i] = ord(w[i])
+        
+    target = sum(w)
+    
+    total = sum(s[:m])
+    if total == target:
+        print("YES")
+        continue
+    flag = False
+    left = 0
+    for right in range(m, n):
+        total += s[right]
+        total -= s[left]
+        if total == target:
+            flag = True
+            break
+        left += 1
+        
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+    
+    
+    
+    
+    
