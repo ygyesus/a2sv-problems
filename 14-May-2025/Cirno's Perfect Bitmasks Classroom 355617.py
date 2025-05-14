8229eb44ff81cd0a87961d2ae75294ed821a8b7e
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-05-14 20:08:58 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 14-May-2025/Cirno's Perfect Bitmasks Classroom 355617.py
@@ -0,0 +1,18 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    y = 1
+    
+    while True:
+        if x&y and x^y:
+            print(y)
+            break
+        
+        if x&(y+1) and x^(y+1):
+            print(y+1)
+            break      
+        y <<= 1
+        
