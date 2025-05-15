a873d4531a46fbc33fe8102f4df6863e7b742df1
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-05-15 19:00:20 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 15-May-2025/Single Number 356394.py
@@ -0,0 +1,10 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        res = 0
+        for num in nums:
+            res ^= num
+
+        return res
