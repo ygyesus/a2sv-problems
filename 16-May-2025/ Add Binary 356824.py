0ea1bb99a86be59fffb7c98ed547ad3fbe0fc946
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-05-16 10:00:40 +0300

    Created ' Add Binary' by A2SV Hub

---
+++ 16-May-2025/ Add Binary 356824.py
@@ -0,0 +1,8 @@
+# Problem:  Add Binary - https://leetcode.com/problems/add-binary/
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        
+        a = int(a,2)
+        b = int(b,2)
+        return bin(a+b)[2:]
