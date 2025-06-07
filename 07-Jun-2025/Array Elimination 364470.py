9878771c3c041beb5997ade749763eed426f30c2
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-06-07 11:51:21 +0300

    Created 'Array Elimination' by A2SV Hub

---
+++ 07-Jun-2025/Array Elimination 364470.py
@@ -0,0 +1,36 @@
+# Problem: Array Elimination - https://codeforces.com/contest/1601/problem/A
+
+from collections import Counter
+t = int(input())
+for _ in range(t):
+    counter = Counter()
+
+    n = int(input())
+    
+    a = list(map(int, input().split()))
+    
+    for i in range(n):
+#         print(bin(a[i])[2:])
+        
+        bitLength = 0
+        while a[i]>>bitLength:
+            counter[bitLength] += a[i]>>bitLength & 1
+            bitLength += 1
+            
+    ans = []
+#     print(dict(counter))
+    for k in range(1, n+1):
+        flag = True
+        for bitLength in counter:
+            if counter[bitLength]%k:
+                flag = False
+                break
+            
+        if flag:
+            ans.append(k)
+            
+    print(*ans)
+        
+    
+    
+    
