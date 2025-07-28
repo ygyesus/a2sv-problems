f7d44d3bcb6d17430e886100dc0f861ee5b0b37d
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-07-28 21:37:20 +0300

    Created 'Percentage of Letter in String' by A2SV Hub

---
+++ 28-Jul-2025/Percentage of Letter in String 380055.py
@@ -0,0 +1,5 @@
+# Problem: Percentage of Letter in String - https://leetcode.com/problems/percentage-of-letter-in-string/description/%20meaning/
+
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        return (s.count(letter)*100)//len(s)
