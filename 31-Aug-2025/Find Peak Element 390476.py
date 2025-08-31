259c7481a1b8cd8c67a97c0d7afbcbadab6c7246
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-08-31 22:51:49 +0300

    Created 'Find Peak Element' by A2SV Hub

---
+++ 31-Aug-2025/Find Peak Element 390476.py
@@ -0,0 +1,16 @@
+# Problem: Find Peak Element - https://leetcode.com/problems/find-peak-element
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        
+        left, right = 0, len(nums)-1
+
+        while left<=right:
+            mid = (left+right)//2
+
+            if mid>0 and nums[mid] < nums[mid-1]:
+                right = mid-1
+            elif mid<len(nums)-1 and nums[mid] < nums[mid+1]:
+                left = mid+1
+            else:
+                return mid
