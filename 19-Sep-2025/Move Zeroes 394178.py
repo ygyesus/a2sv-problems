233d2929ebc5cc01eb5d3fe794d7bb64b2ee3598
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-09-19 19:29:07 +0300

    Created 'Move Zeroes' by A2SV Hub

---
+++ 19-Sep-2025/Move Zeroes 394178.py
@@ -0,0 +1,14 @@
+# Problem: Move Zeroes - https://leetcode.com/problems/move-zeroes/
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
