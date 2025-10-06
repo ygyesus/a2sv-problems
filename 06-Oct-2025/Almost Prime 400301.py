b735e5431808a70dd37b16425abe2cf712e53773
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-10-06 19:45:38 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 06-Oct-2025/Almost Prime 400301.py
@@ -0,0 +1,20 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def isAlmostPrime(num):
+    temp = num
+    factorization = set()
+    for d in range(2, temp):
+        if not d*d<=temp: break
+        while num%d == 0:
+            num //= d
+            factorization.add(d)
+            
+    if num>1: factorization.add(num)
+    return len(factorization) == 2
+
+
+n = int(input())
+ans = 0
+for num in range(2, n+1):
+    ans += isAlmostPrime(num)
+print(ans)
