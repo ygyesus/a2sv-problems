aa28f612d06a444ee87eb6c5b5afa392b2b2b3a9
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-10-18 18:42:49 +0300

    Created 'Contains Duplicate' by A2SV Hub

---
+++ 18-Oct-2025/Contains Duplicate 404248.py
@@ -0,0 +1,10 @@
+# Problem: Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for num in nums:
+            if num in visited: return True
+            visited.add(num)
+
+        return False
