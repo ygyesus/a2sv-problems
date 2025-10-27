34712e7d0b3b2cc55d9025ad8cdf32c1c987d5a1
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-10-27 18:56:27 +0300

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 27-Oct-2025/Dreamoon and Stairs 405998.py
@@ -0,0 +1,25 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+totalSteps, moveFactor = map(int, input().split())
+
+def isValid(moves):
+    if not moves%moveFactor == 0: return False
+    
+    return 1*moves <= totalSteps <= 2*moves
+
+
+left, right = 0, 10**4
+
+ans = -1
+while left<=right:
+    k = (left+right)//2
+    moves = moveFactor*k
+    if isValid(moves):
+        ans = moves
+        right = k-1
+    elif 2*moves < totalSteps:
+        left = k+1
+    elif totalSteps < moves:
+        right = k-1
+        
+print(ans)
