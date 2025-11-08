31b6c0dfc32271f4c7500efe6cc5ec7952360db3
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-11-09 02:07:05 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 08-Nov-2025/Fox And Snake 408057.py
@@ -0,0 +1,19 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+rows, cols = map(int, input().split())
+
+grid = [
+    ['.' for _ in range(cols)] for _ in range(rows)
+]
+for row in range(0, rows, 2):
+    
+    for col in range(cols):
+        grid[row][col] = '#'
+        
+right = True
+for row in range(1, rows, 2):
+    if right: grid[row][-1] = '#'
+    else: grid[row][0] = '#'
+    right = not right
+    
+for ROW in grid: print(''.join(ROW))
