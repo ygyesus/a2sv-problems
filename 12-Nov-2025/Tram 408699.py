d0c1ead5726249b344bca7231190de61846121d8
Yosef G <126314080+ygyesus@users.noreply.github.com>
2025-11-13 01:29:32 +0300

    Created 'Tram' by A2SV Hub

---
+++ 12-Nov-2025/Tram 408699.py
@@ -0,0 +1,11 @@
+# Problem: Tram - https://codeforces.com/problemset/problem/116/A
+
+n = int(input())
+
+maxAns = ans = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    ans += (b-a)
+    maxAns = max(maxAns, ans)
+    
+print(maxAns)
